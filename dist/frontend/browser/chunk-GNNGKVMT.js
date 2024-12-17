d6826cb7659227c14c0b8da220fae0a87d5b7ef6
myommt <zai.myo.mintun@gmail.com>
2024-12-17 14:05:34 -0600

    add public

---
+++ dist/frontend/browser/chunk-GNNGKVMT.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>import("./chunk-2SHYG4YX.js").then(o=>o.BudgetlistComponent)},{path:"new",loadComponent:()=>import("./chunk-CBOR6VFL.js").then(o=>o.BudgetformComponent)},{path:":budget_id",loadComponent:()=>import("./chunk-GPLTQ4RT.js").then(o=>o.BudgeteditComponent)}];export{t as budget_routes};
---
+++ dist/frontend/browser/chunk-O3I2PTIJ.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var e=[{path:"",loadComponent:()=>import("./chunk-RBM3K6BM.js").then(o=>o.ExpenselistComponent)},{path:"new",loadComponent:()=>import("./chunk-6Y5PQ5YY.js").then(o=>o.ExpenseformComponent)},{path:":expense_id",loadComponent:()=>import("./chunk-RMWKRYSN.js").then(o=>o.ExpenseeditComponent)}];export{e as expenses_routes};
